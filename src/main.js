b79b862c7b0ccc666ffa367b6338952daf093911
chaoshuai <chaoshuai@takecloud.cn>
2018-07-13 18:25:55 +0800

    使用vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import iView from 'iview'
+import router from './router'
+import store from './store'
 import 'iview/dist/styles/iview.css'
 import http from './utils/request'
 import Cookies from 'js-cookie'
@@ -38,6 +39,7 @@ Vue.config.productionTip = false
 new Vue({
     el: '#app',
     router,
+    store,
     components: {
         App
     },
---
+++ src/store/modules/user.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import vuex from 'vuex'
+
+import app from './modules/app'
+import user from './modules/user'
+
+Vue.use(Vuex)
+
+
+export default tnew Vuex.Store({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        // app,
+        // user
+    } 
+})
